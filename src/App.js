6be1de200c607bc1ad53e1c3d2224cc8063a49ef
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-10-24 08:54:33 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from 'react';
+import styles from './App.module.css'
 function App() {
+  const [data, setData] = useState()
+
+  const formatter = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  useEffect(() => {
+    fetch('https://alunos.treinaweb.com.br/hyperprof/api/professores', {
+      method: 'GET',
+    }).then((response) => response.json()).then((data) => setData(data)).catch(() => setData([]))
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header className={styles.header}>
+        <img className={styles.image} src="/myteacher.png" alt="my teacher" />
       </header>
+      <main className={styles.container}>
+        <h1>Encontre o professor perfeito</h1>
+        {data ? (
+          data.length === 0 ? <p>{'Nenhum professor disponível'}</p> : (
+            <div className={styles.box_card}>
+              {data?.map((professor) => {
+                return (
+                  <div className={styles.card}>
+                    <img className={styles.image_card} src={professor.foto_perfil ?? '/person.png'} alt={professor.nome} />
+                    <div className={styles.conteudo_card}>
+                      <div>
+                        <h3 className={styles.title}>{professor.nome}</h3>
+                        <span> {formatter.format(professor.valor_aula)}</span>
+                        <p className={styles.descricao}>
+                          {professor.descricao}
+                        </p>
+
+                      </div>
+                      <button className={styles.button}>Marcar aula com {professor.nome}</button>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          )
+        ) : (
+          <div sx={{ textAlign: "center" }}>
+            <div>Carregando...</div>
+          </div>
+        )}
+      </main>
+      <footer style={{ textAlign: 'center' }}>
+        <p>copyrigth <span>&#169;</span>2022</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+
